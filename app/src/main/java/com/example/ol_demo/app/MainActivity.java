f356e5680b8dd74e0a2fc62b046d03b929f82e2e
rubywiwi <wildchen@whu.edu.cn>
2015-04-25 22:09:18 +0800

    demo

---
+++ app/src/main/java/com/example/ol_demo/app/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.ol_demo.app;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.View;
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        webView.loadUrl("file:///android_asset/ol3_test.html");
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+}
